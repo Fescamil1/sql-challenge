6e16b0dd04d2e89b97659a5e50830ffcfead570c
Francisca Escamilla <francis.escamilla@gmail.com>
2021-06-25 23:44:37 -0500

    Corrected title_id and dept_no as they should have been strings and not integers. completed importing of the csv files to each of the tables

--- EmployeeSQL/employee_tables_schema.sql
+++ EmployeeSQL/employee_tables_schema.sql
@@ -1,22 +1,22 @@
 -- Create tables for Employee SQl Challenge
--- Before creating a table, drop the table if it already exsists
-
-
--- drop table if already exsist
+-- Before creating a table, drop the tables if it already exsists
+DROP TABLE dept_manager;
+DROP TABLE dept_emp;
+DROP TABLE departments;
+DROP TABLE salaries;
+DROP TABLE employees;
 DROP TABLE titles;
 
 --create titles table
 CREATE TABLE titles( 
-	title_id integer PRIMARY KEY NOT NULL,
+	title_id varchar PRIMARY KEY NOT NULL,
     title varchar  NOT NULL
 );
 
 -- create a table of employees
-DROP TABLE employees;
-
 CREATE TABLE employees(
 	emp_no integer PRIMARY KEY NOT NULL,
-    emp_title_id integer NOT NULL,
+    emp_title_id varchar NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
     birth_date date NOT NULL,
     first_name varchar NOT NULL,
@@ -26,38 +26,29 @@ CREATE TABLE employees(
 );
 
 --create table for salaries
-DROP TABLE salaries;
-
 CREATE TABLE salaries(
     emp_no integer NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     salary integer NOT NULL
 );
 
-
 --create departments table
-DROP TABLE departments;
-
 CREATE TABLE departments (
-    dept_no integer PRIMARY KEY NOT NULL,
+    dept_no varchar PRIMARY KEY NOT NULL,
     dept_name varchar NOT NULL
 );
 
 -- Create table for dept employees
-DROP TABLE dept_emp;
-
-CREATE TABLE "dept_emp" (
+CREATE TABLE dept_emp (
     emp_no integer NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
-    dept_no integer NOT NULL,
+    dept_no varchar NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
 );
 
 --Create table for dept managers
-DROP TABLE dept_manager;
-
 CREATE TABLE dept_manager (
-    dept_no integer NOT NULL,
+    dept_no varchar NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
     emp_no integer NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
