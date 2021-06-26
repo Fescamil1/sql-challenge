fd8c4e5768b68fb4a5c71ec16cf2723c455568cf
Francisca Escamilla <francis.escamilla@gmail.com>
2021-06-26 00:49:52 -0500

    the queries file did not have all the queries saved properly savinga again

--- EmployeeSQL/Employee_queries.sql
+++ EmployeeSQL/Employee_queries.sql
@@ -13,4 +13,46 @@ FROM employees
 WHERE DATE_PART('year',hire_date)=1986
 ORDER BY emp_no;
 
---
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT  m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
+FROM dept_manager as m
+LEFT JOIN departments as d on m.dept_no = d.dept_no
+LEFT JOIN employees as e on m.emp_no = e.emp_no;
+
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+LEFT JOIN dept_emp as d_e on e.emp_no = d_e.emp_no
+LEFT JOIN departments as d on d_e.dept_no = d.dept_no
+ORDER BY e.emp_no;
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex 
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+LEFT JOIN dept_emp as d_e ON e.emp_no = d_e.emp_no
+LEFT JOIN departments as d ON d_e.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+ORDER BY e.emp_no;
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+LEFT JOIN dept_emp as d_e ON e.emp_no = d_e.emp_no
+LEFT JOIN departments as d ON d_e.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+ORDER BY e.emp_no;
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name)as frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
+
